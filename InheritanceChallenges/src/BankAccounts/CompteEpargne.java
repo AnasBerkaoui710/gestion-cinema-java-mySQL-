66d6ae6e993b5a35c264043bb4330212f4532630
anas berkaoui <berkaoui.anass12@gmail.com>
2025-11-20 10:12:11 +0100

    créér la classe compteEpargne

--- InheritanceChallenges/src/BankAccounts/CompteEpargne.java
+++ InheritanceChallenges/src/BankAccounts/CompteEpargne.java
@@ -1,4 +1,18 @@
 package BankAccounts;
 
-public class CompteEpargne {
+public class CompteEpargne extends Compte {
+    double tauxInteret;
+
+    CompteEpargne(int numero,double solde, double tauxInteret){
+        super(numero, solde);
+        this.tauxInteret=tauxInteret;
+
+    }
+
+    public double appliquerInteret(){
+        return solde += solde * tauxInteret;
+    }
+
+
+
 }
