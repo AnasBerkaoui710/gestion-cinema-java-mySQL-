32ffd8252221c9896bfea05a50d565f8bee039e5
anas berkaoui <berkaoui.anass12@gmail.com>
2025-11-20 10:11:39 +0100

    override the method retirer et verser

--- InheritanceChallenges/src/BankAccounts/CompteCourant.java
+++ InheritanceChallenges/src/BankAccounts/CompteCourant.java
@@ -5,6 +5,27 @@ public class CompteCourant extends Compte {
 
     CompteCourant(int numero, double solde){
         super(numero, solde);
+        this.limiteDecouvert=limiteDecouvert;
+    }
+
+    @Override
+    public double verser(double montant){
+        System.out.println("versemment effectué avac suucees!!");
+        return solde += montant;
+    }
+    
+
+
+    @Override
+    public double retirer(double montant){
+        if(solde-montant>limiteDecouvert){
+            System.out.println("retirement effectué avac suucees!!");
+            return solde-=montant;
+
+        }else{
+            System.out.println("vous avez atteit la limite decouvert!!!");
+            return solde;
+        }
     }
 
 }
