b35bb132c05b9cef25b1bc2bba0fc66f07539ae7
anas berkaoui <berkaoui.anass12@gmail.com>
2025-11-26 11:43:24 +0100

    add the necessery tables to the database

--- gestionCinema/src/script.sql
+++ gestionCinema/src/script.sql
@@ -1,1 +1,39 @@
 create database cinema;
+
+use cinema;
+
+CREATE TABLE film (
+    id_film INT AUTO_INCREMENT PRIMARY KEY,
+    titre VARCHAR(50) NOT NULL,
+    duree INT NOT NULL,
+    categorie VARCHAR(100)
+);
+
+CREATE TABLE seance (
+	id INT AUTO_INCREMENT PRIMARY KEY,
+	horaire INT NOT NULL,
+    salle VARCHAR(50),
+    capacite_max DOUBLE
+);
+
+CREATE TABLE spectateur(
+	id_spectateur INT AUTO_INCREMENT PRIMARY KEY,
+    nom VARCHAR(50) NOT NULL,
+    email VARCHAR(100)
+);
+
+CREATE TABLE tickets(
+	num INT PRIMARY KEY,
+    prix DOUBLE NOT NULL
+);
+
+CREATE TABLE reservation(
+	id_reser INT AUTO_INCREMENT PRIMARY KEY,
+    date_reserv VARCHAR(50),
+    id_spectateur int,
+    id_seance int,
+    foreign key (id_spectateur) references spectateur (id_spectateur),
+    foreign key (id_seance) references seance (id)
+);
+
+
