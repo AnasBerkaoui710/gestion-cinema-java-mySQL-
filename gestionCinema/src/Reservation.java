e0bca3a5e571e93f91ae026bc5ac524de196ba9b
hayat <lahlalihayat70@gmail.com>
2025-11-28 12:04:20 +0100

    Work in progress: update Reservation, Seance, Spectateur, Ticket

--- gestionCinema/src/Reservation.java
+++ gestionCinema/src/Reservation.java
@@ -1,4 +1,45 @@
-package PACKAGE_NAME;
-
 public class Reservation {
+    private  int id_reser;
+    private String date_reserv;
+    private  Seance seance;
+    private Spectateur spectateur;
+
+    public Reservation(int id_reser,String date_reserv , Seance seance, Spectateur spectateur) {
+        this.id_reser = id_reser;
+        this.date_reserv = date_reserv;
+        this.seance = seance;
+        this.spectateur = spectateur;
+    }
+
+    public String getDate_reserv() {
+        return date_reserv;
+    }
+
+    public void setDate_reserv(String date_reserv) {
+        this.date_reserv = date_reserv;
+    }
+
+    public int getId_reser() {
+        return id_reser;
+    }
+
+    public void setId_reser(int id_reser) {
+        this.id_reser = id_reser;
+    }
+
+    public Seance getSeance() {
+        return seance;
+    }
+
+    public void setSeance(Seance seance) {
+        this.seance = seance;
+    }
+
+    public Spectateur getSpectateur() {
+        return spectateur;
+    }
+
+    public void setSpectateur(Spectateur spectateur) {
+        this.spectateur = spectateur;
+    }
 }
--- gestionCinema/src/Seance.java
+++ gestionCinema/src/Seance.java
@@ -1,4 +1,44 @@
-package PACKAGE_NAME;
-
 public class Seance {
+    private  String horraire;
+    private String salle;
+    private double capacite_max;
+    private Film film;
+    public Seance(String horraire,String salle,double capacite_max,Film film){
+        this.horraire=horraire;
+        this.salle=salle;
+        this.capacite_max=capacite_max;
+        this.film=film;
+    }
+
+    public double getCapacite_max() {
+        return capacite_max;
+    }
+
+    public void setCapacite_max(double capacite_max) {
+        this.capacite_max = capacite_max;
+    }
+
+    public Film getFilm() {
+        return film;
+    }
+
+    public void setFilm(Film film) {
+        this.film = film;
+    }
+
+    public String getHorraire() {
+        return horraire;
+    }
+
+    public void setHorraire(String horraire) {
+        this.horraire = horraire;
+    }
+
+    public String getSalle() {
+        return salle;
+    }
+
+    public void setSalle(String salle) {
+        this.salle = salle;
+    }
 }
